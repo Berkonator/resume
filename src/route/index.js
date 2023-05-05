84c8ec36e7a1fc3da2d17cb6d9dd4c11308eb0df
Berkonator <ryurechko@gmail.com>
2023-05-06 00:42:38 +0200

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -2,6 +2,37 @@
 const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
+//================================================================
+
+var header = {
+  name: {
+    firstname: 'Roman',
+    lastname: 'Yurechko',
+  },
+
+  position: 'Junior Fullstack JS Developer',
+
+  salary: '600$ в місяць',
+
+  address: 'Вул. Болотна, Nr. Заляпаний',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'ryurechko@gmail.com',
+      href: 'mailto:reyurechko@gmail.com',
+    },
+    phone: {
+      text: '+48-795-650-875',
+      href: 'tel:+48795650875',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/roman-yurechko-a1a00126b/',
+    },
+  },
+}
 
 // ================================================================
 
@@ -23,6 +54,198 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.`,
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: ` Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
+  })
+})
+
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'Handlbars',
+          point: 8,
+          isTop: false,
+        },
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'VS Code & NPM',
+          point: 9,
+          isTop: true,
+        },
+        {
+          name: 'Git & Terminal',
+          point: 7,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        {
+          name: 'spend time with family',
+          isMain: true,
+        },
+        {
+          name: 'play chess',
+          isMain: false,
+        },
+        {
+          name: 'travel',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      education: [
+        {
+          name: 'IFNTUNG',
+          isEnd: true,
+        },
+        {
+          name: 'PUNAVS',
+          isEnd: true,
+        }, //Prykarpattia University named after Vasyl Stefanyk
+        {
+          name: 'Kyiv-Mohyla Academy',
+          isEnd: false,
+        },
+      ],
+      certificates: [
+        {
+          name: 'permission to drive a forklift',
+          id: 785634,
+        },
+        {
+          name: `welder's permit`,
+          id: 456537,
+        },
+        {
+          name: 'tolerance up to 1 kilovolt',
+          id: 210497,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Deceloper',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua',
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmount: 3,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://resume.com.ua',
+              about:
+                'Airbnb competitor. High-load application for searching apartments',
+
+              stackAmount: '3',
+
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Node.js',
+                },
+              ],
+
+              awordAmount: '2',
+
+              awards: [
+                {
+                  name: 'Background verificaion - is a faeture that provides users to prove that they are real persons',
+                },
+                {
+                  name: 'Preparing SEO optimized pages. The automated process of getting data for the app from documents.',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
   })
 })
 
